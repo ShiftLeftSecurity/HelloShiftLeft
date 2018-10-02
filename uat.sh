71f35a5fea096e9e3bb1a031ce17050f9746fdc0
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-02 14:54:23 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -1,5 +1,5 @@
 sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
-sleep 20
+sleep 60
 curl http://localhost:8081/customers/1
 curl http://localhost:8081/customers/
 curl http://localhost:8081/patients
