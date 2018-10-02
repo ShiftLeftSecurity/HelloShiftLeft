d8d2f504d40d9e8e4cb89010cfe7995a1d692e34
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-02 14:44:06 -0700

    Create uat.sh

---
+++ uat.sh
@@ -0,0 +1,8 @@
+sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
+sleep 20
+curl http://localhost:8081/customers/1
+curl http://localhost:8081/customers/
+curl http://localhost:8081/patients
+curl http://localhost:8081/account
+curl http://localhost:8081/account/1
+sleep 20
