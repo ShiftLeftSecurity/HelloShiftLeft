6f150fe4f7509f6b6c09a3724c55bde93fe49650
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-19 01:04:31 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -1,3 +1,5 @@
+export SHIFTLEFT_SEC_BLOCK_ATTACKS=$1
+echo "Running with blocking mode: $1"
 sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
 sleep 60
 curl http://localhost:8081/customers/1
@@ -5,4 +7,5 @@ curl http://localhost:8081/customers/
 curl http://localhost:8081/patients
 curl http://localhost:8081/account
 curl http://localhost:8081/account/1
+http://localhost:8081/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo $SHIFTLEFT_ORG_TOKEN>/tmp/hacked'}).start()
 sleep 20
