94fdc4d7568587f3b2245132f1f8bcd3a00ccad6
Prathik Gadde <pratheekgadde@gmail.com>
2019-04-09 10:16:54 -0700

    fix: acc changes

--- src/main/java/io/shiftleft/Application.java
+++ src/main/java/io/shiftleft/Application.java
@@ -2,7 +2,7 @@ package io.shiftleft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//wololo
 @SpringBootApplication
 public class Application {
 
