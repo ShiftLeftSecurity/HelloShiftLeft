4d1696c2206eb060a3942dbc1934b7105f2692f1
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2020-03-01 16:05:24 -0800

    Update actionanalysis.sh

--- actionanalysis.sh
+++ actionanalysis.sh
@@ -9,7 +9,7 @@ echo "Got pull request $PULL_REQUEST for branch $GITHUB_BRANCH"
 # Install ShiftLeft
 curl https://www.shiftleft.io/download/sl-latest-linux-x64.tar.gz > /tmp/sl.tar.gz && sudo tar -C /usr/local/bin -xzf /tmp/sl.tar.gz
 
-sl analyze --version-id "$GITHUB_SHA" --tag branch="$GITHUB_BRANCH" --app "$GITHUB_PROJECT" --cpg --wait --force target/hello-shiftleft-0.0.1.jar
+sl analyze --version-id "$GITHUB_SHA" --tag branch="$GITHUB_BRANCH" --app "$GITHUB_PROJECT" --cpg --wait --force hello-shiftleft-0.0.1.jar
 
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
